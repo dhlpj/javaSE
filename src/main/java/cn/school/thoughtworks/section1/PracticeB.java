8ff71feec1e3d71b9e4de7f5beec38a38c75ebe2
Jack <773411997@qq.com>
2019-05-16 17:56:42 +0800

    finish section1 practiseBTest

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,14 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> collection2New = collection2.stream().flatMap(Collection::stream).collect(Collectors.toList());//抽取底层元素
+        return collection1.stream().filter(collection2New::contains).collect(Collectors.toList());
     }
 }
