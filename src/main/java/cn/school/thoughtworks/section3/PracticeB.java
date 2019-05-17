7294b94a0ab17a1460192056e8c1f775243605d7
Jack <773411997@qq.com>
2019-05-17 10:08:14 +0800

    finish section3 practiceBTest

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -6,7 +6,13 @@ import java.util.Map;
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        List<String> value = object.get("value");
+        collectionA.entrySet().forEach(entry->{
+            String key = entry.getKey();
+            if(value.contains(key)){
+                entry.setValue(entry.getValue()-entry.getValue()/3);
+            }
+        });
+        return collectionA;
     }
 }
